c728d847db25c6250cc2a2e402b792e8e03dec5e
arnold jones <tosspinc@gmail.com>
2024-07-26 15:43:11 -0600

    updated the issueRouter.js to allow for editing and deleting an issue with the user id. completed 07.26.2024 at 1544hours.

--- Level6/aconxn/Routes/issueRouter.js
+++ Level6/aconxn/Routes/issueRouter.js
@@ -26,15 +26,50 @@ issueRouter.get('/', async (req, res, next) => {
     }
 });
 
+//edit an issue
+issueRouter.put('/post/:issueId', async (req, res, next) => {
+    try {
+    const updatedIssue = await Issue.findByIdAndUpdate (
+        req.params.issueId,
+        req.body,
+        { new: true}
+    );
+    if (!updatedIssue) {
+        res.status(404)
+        return next(new Error('Issue not found.'))
+    }
+    return res.status(200).send(updatedIssue)        
+    } catch (error) {
+        res.status(500)
+        return next(error)
+    }
+})
+
 // Get all issues
 issueRouter.get('/all', async (req, res, next) => {
     try {
-        const allIssues = await Issue.find(); // Corrected: 'Issue'
+        const allIssues = await Issue.find(); 
         return res.status(200).send(allIssues);
     } catch (error) {
         res.status(500);
         return next(error);
     }
 });
+
+//delete an issue.
+issueRouter.delete('/:issueId', async (req, res, next) => {
+    try {
+        const deletedIssue = await Issue.findByIdAndDelete(req.params.issueId)
+
+        if (!deletedIssue) {
+            return res.status(400).send({ message: 'No Issue has been found to delete.' })
+        }
+
+        return es.status(200).send({ message: `Issue titled ${deletedIssue.title} has been deleted successfully.` })
+    } catch (error) {
+        res.status(500)
+        return next(error)
+    }
+})
 
 module.exports = issueRouter;
